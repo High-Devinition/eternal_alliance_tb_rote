7935aa48d15d9311e743a3c7b2a909d7e5567f5e
High-Devinition <High-Devinition@outlook.com>
2023-07-24 22:42:16 -0400

    Many changes

--- js/planets.js
+++ js/planets.js
@@ -1,51 +1,55 @@
 
 
-//	alert("Hello world!");
+import { computed } from "vue"
 
-//	.on("mouseenter", 
+/**
+ * 
+ * @param {*} el_target_ref The Root element for which clicking outside will trigger callback_fn
+ * @param {*} on_click_outside The function to call when user clicks outside of
+ * @param {Function} callback_condition Function, if provided, returns boolean indication if click outside should be allowed to happen
+ * @returns 
+ */
+export async function useClickOutside(el_target_ref, on_click_outside, callback_condition) {
+    if (!el_target_ref) return;
+    if (!el_target_ref.value) {
+        console.log("useClickOutside", "target element was not supplied or is null")
+        //return 
+    }
 
-//	$(this).attr("background-color", "black");
-//		}
-
-//	("v-bind:src", "'../media/map_ui/mission_' + mission.type + '_focused' + '.png'");
-
-//$('.mission')
-//	.on('mouseleave'), function () {
-//		$(this).css('v-bind:src', $(this).attr("'../media/map_ui/mission_' + mission.type + '.png'"));
-
-
-
-
-$(document).ready(function () {
-
-
-		$('img.mission').on('mouseover'), function () {
-			$(this).attr('v-bind:src', "'../media/map_ui/mission_' + mission.type + '_focused' + '.png'");
-		}
-	
-
-
-		$('img.mission').on('mouseout'), function () {
-			$(this).attr('v-bind:src', "'../media/map_ui/mission_' + mission.type + '.png'");
-		}
-	
-
-});
-
-//$("#outer").on("mouseenter", function () {
-//	$("#log").append("<div>Handler for `mouseenter` called.</div>");
-//});
-
-//$('.mission').on('mouseover'), function () {
-//	$(this).attr('v-bind:src', "'../media/map_ui/mission_' + mission.type + '_focused' + '.png'");
+    var dont_use_first_click = 0;
+    let listener = async (e) => {
+        var enable_click_outside = true;
+        if (typeof callback_condition == 'function') {
+            enable_click_outside = await callback_condition();
+        }
+        if (!enable_click_outside) return;
 
 
+        if (dont_use_first_click == 0) {
+            dont_use_first_click++;
+            return;
+        }
+        if (e.target == el_target_ref.value || e.composedPath().includes(el_target_ref.value)) {
+            //We clicked inside the modal/active region
+            return
+        }
 
-	//$('img .mission')
-	//	.on('mouseenter', function () {
-	//		$(this).attr('v-bind:src', "'../media/map_ui/mission_' + mission.type + '_focused' + '.png'");
-	//	},
-	//			$('.mission')
-	//			.on('mouseleave'), function () {
-	//				$(this).css('v-bind:src', $(this).attr("'../media/map_ui/mission_' + mission.type + '.png'"));
-	//			});
+
+        //at this point we clicked outside the modal
+        if (enable_click_outside && typeof on_click_outside == 'function') {
+            on_click_outside();
+
+        }
+    }
+
+
+    onMounted(() => {
+        window.addEventListener('click', listener)
+    })
+    onBeforeUnmount(() => {
+        window.removeEventListener('click', listener)
+    })
+    return {
+        listener
+    }
+}
